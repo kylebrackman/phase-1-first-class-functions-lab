955166437ab8f39abaa94ba4c1e7b33e9ff76df0
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-02 17:26:48 -0600

    commit tree 8e9210750643ad61e6a7206c9fb943f2da9bc4cc

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution in this file!
+// practice below
+
+const returnFirstTwoDrivers = function (x) {
+    return x.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function (x) {
+    return x.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (x) {
+     return (fare) => fare * x
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(driverList, y) {
+    return y(driverList)
+}
+
+
+
+
+
+
+
+
+// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+// const returnFirstTwoDrivers = function(x) {
+//     return x.slice(0,2);
+// }
+// returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+// const returnLastTwoDrivers = function(x) {
+//     return x.slice(2);
+// }
+// returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier(fare) {
+//     return function(x) {
+//         return x * fare
+//     }
+// }
+// const fareDoubler = createFareMultiplier(2);
+// const fareTripler = createFareMultiplier(3);
+
+// function selectDifferentDrivers(drivers, y) {
+//     return y(drivers)
+// }
